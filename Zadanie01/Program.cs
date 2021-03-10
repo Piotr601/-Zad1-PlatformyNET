e02d6bfd18184d7620eacc60d6bf614bf2795a7f
Piotr601 <piotr.niedziolka601@gmail.com>
2021-03-10 14:46:00 +0100

    Wersja Startowa 1.0 Wczytywanie danych i losowanie

--- Zadanie01/Program.cs
+++ Zadanie01/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GeneratorCS;
 
 namespace Zadanie01
 {
@@ -10,7 +11,35 @@ namespace Zadanie01
     {
         static void Main(string[] args)
         {
-         
+
+            Console.WriteLine("Podaj ile obiektow chcesz wylosowac:");
+            int obiekty = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj ile miejsca ma miec plecak:");
+            int pojemnosc = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj dowolny seed:");
+            int seed = int.Parse(Console.ReadLine());
+
+            RandomNumberGenerator rng = new RandomNumberGenerator(seed); //tutaj tworzymy obiektb
+
+            Console.WriteLine("PROGRAM:");
+
+            int[] tablica = new int[obiekty];
+
+            for (int i=0; i<obiekty; i++)
+            {
+                tablica[i] = rng.nextInt(1, 30);
+
+                Console.WriteLine(tablica[i]);
+            }
+
+            // SPRAWDZNIE 
+            Console.WriteLine("Sprawdzenie");
+            Console.WriteLine("Obiekty = {0:D}", obiekty);
+            Console.WriteLine("Pojemnosc = {0:D}", pojemnosc);
+            Console.WriteLine("Seed = {0:D}", seed);
+            Console.WriteLine("Koniec programu");
         }
     }
 }
