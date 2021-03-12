78ed2317e33389a145dad3d4b2fde39357cf8493
Filip Kurasz <filipkurasz99@gmail.com>
2021-03-12 11:08:20 +0100

    Zmieniona nazwa testow

--- TestProject/TestyProblemPlecakowy.cs
+++ TestProject/TestyProblemPlecakowy.cs
@@ -2,7 +2,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Zadanie01;
 using System;
 
-namespace TestProject
+namespace TestZadanie01
 {
     [TestClass]
     public class TestyProblemPlecakowy
